abbbe5f659c8f10fad9a016d9221afeaa986bd36
roopkishor sharma <rajoriyark04@gmail.com>
2026-09-09 22:57:17 +0530

    chore: add .next to gitignore and remove tracked build files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xai77wpFtZvxjiIlA/PB0wroJMGVQfoMF0ABjYM155w=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oCM2r8Nr07V9fcLTlXs3ZsX9cTc22MTRrizMQNf1sIQ=\"\n}"
